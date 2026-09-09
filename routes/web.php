b1a80986d1783ee37ec333b06bb3e58368229877
B1eL <engs-gabrielcaldas@camporeal.edu.br>
2026-09-09 18:51:16 -0300

    ATV 2: Rotas com parâmetros

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::get('/', function () {
 Route::get('/sobre', function () { return 'Página Sobre'; });
 Route::get('/alunos', function () { return 'Página de Alunos'; });
 Route::get('/contato', function () { return 'Página de Contato'; });
+Route::get('/produto/{id}', function ($id) { return "Produto: {$id}"; });
+Route::get('/categoria/{id}', function ($id) { return "Categoria: {$id}"; });
+Route::get('/usuario/{id}', function ($id) { return "Usuário: {$id}"; });
