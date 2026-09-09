4db839e7a8902cbfc569cb1fd7e542cab0ee46b1
B1eL <engs-gabrielcaldas@camporeal.edu.br>
2026-09-09 18:58:38 -0300

    ATV 7: Crie um layout para ser utilizado por outras páginas em '/layouts/app.blade.php'

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sistema</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a> | 
+        <a href="/alunos">Alunos</a>
+    </nav>
+    
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
